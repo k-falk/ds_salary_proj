56fb5609b0310739bba4683db158f53aa4fc79f6
k-falk <xkfalkx@gmail.com>
2020-10-05 23:15:51 -0400

    Did some exploratory data analysis

--- data_cleaning.py
+++ data_cleaning.py
@@ -6,13 +6,13 @@ Created on Thu Oct  1 00:27:25 2020
 """
 import pandas as pd
 
+
 df = pd.read_csv("job_data.csv")
 
 ## Couple of things we need to do here to clean data
 ## We need to remove rows that are no longer shown on glassdoor ie: headquarters and competitors
 ## Then we need to fix some things with our data ie: fix company name
 ## Then we will make our data a little more useful to us
-df1 = df
 
 
 
@@ -36,16 +36,81 @@ df.Age = df.Age.replace({2021: 0})
 
 
 ##Drop Jobs with no salary estimaate because they are useless to us
-df = df[df['Salary Estimate'] != 1]
+df = df[df['Salary Estimate'] != '-1']
+df = df[~df['Salary Estimate'].str.contains('Employer Provided Salary')]
 
 ##We are going to add estimates as their own columns to make things easier, these will be in thousands
 salary= df['Salary Estimate'].apply(lambda x: x.split("(")[0])
 salary = salary.str.replace("$",'').str.replace("K",'')
+
 df['Salary Estimate'] = salary
 df['Salary Estimate'] = salary
-salary = salary.str.split("-", n = 1, expand = True) 
-df['minSalary'] = salary[0]
-df['maxSalary'] = salary[1]
+
+salary_split = salary.str.split("-", n = 1, expand = True) 
+
+df['min_salary'] = salary_split[0]
+df['max_salary'] = salary_split[1]
+df['average_salary'] = salary_split.astype(float).mean(axis=1)
+
+
+##Lets start categorizing our jobs
+
+
+##Lets parse our the titles
+def title_parser(title):
+    if "data scientist" in title.lower():
+        return "data scientist"
+    elif "analyst" in title.lower():
+        return "analyst"
+    elif "machine learning" in title.lower():
+        return "mle"
+    elif "researcher" in title.lower():
+        return "researcher"
+    elif "manager" in title.lower():
+        return "manager"
+    elif "director" in title.lower():
+        return "director"
+    else: 
+        return "na"
+    
+df["simplified_title"] = df["Job Title"].apply(lambda x: title_parser(x))
+df.simplified_title.value_counts()
+
+def seniority_parser(title):
+    if 'sr' in title.lower() or 'senior' in title.lower() or 'sr' in title.lower() or 'lead' in title.lower() or 'principal' in title.lower():
+        return 'senior' 
+    elif 'jr' in title.lower() or 'jr.' in title.lower() or 'junior' in title.lower() or 'associate' in title.lower():
+        return 'junior'
+    else:
+        return 'na'
+df['seniority'] = df['Job Title'].apply(lambda x: seniority_parser(x))
+df.seniority.value_counts()
+
+def state_parser(loc):
+    split = loc.split(",")
+    if( 'Los Angeles' in loc):
+        return ' CA'
+    elif( 'Maryland' in loc):
+        return ' MD'
+    elif( 'Missouri' in loc):
+        return ' MO'
+    elif( 'Utah' in loc):
+        return ' UT'
+    elif( 'Illinois' in loc):
+        return 'IL'
+    elif( 'New Jersey' in loc):
+        return ' NJ'
+    elif( 'Colorado' in loc):
+        return ' CO'
+    elif( 'California' in loc):
+        return ' CA'
+    elif(len(split) >1):
+        return split[1]
+    elif('United States' in loc):
+        return ' na'
+    else: return split[0]
+df['State']= df['Location'].apply(lambda x: state_parser(x))
+df['State'].value_counts()
 
 ## We should look into job description now
 
@@ -59,7 +124,7 @@ df['java_count'].value_counts()
 df['sql_count'] = df['Job Description'].apply(lambda x: 1 if 'sql' in x.lower() else 0)
 df['sql_count'].value_counts()
 ##R
-df['python_count'] = df['Job Description'].apply(lambda x: 1 if ' r ' in x.lower() else 0)
+df['r_count'] = df['Job Description'].apply(lambda x: 1 if ' r ' in x.lower() else 0)
 df['python_count'].value_counts()
 ##spark
 df['spark_count'] = df['Job Description'].apply(lambda x: 1 if 'spark' in x.lower() else 0)
